6fd0ce2369ce3e1b32f239f36f025662cf2b5ed2
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-11 21:59:03 +1100

    Add basic config

---
+++ server/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type ConfigData struct {
+	Msg string `json:"msg"`
+}
+
+func LoadConfig(path string) (*ConfigData, error) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config ConfigData
+	json_err := json.Unmarshal(bytes, &config)
+	if json_err != nil {
+		return nil, json_err
+	}
+
+	return &config, nil
+}
--- server/main.go
+++ server/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/aixoio/amionline/server/config"
 	"github.com/aixoio/amionline/server/db"
 	"github.com/aixoio/amionline/server/router"
 )
 
 func main() {
+	c, _ := config.LoadConfig("env.json")
+	fmt.Println(c.Msg)
 	d, _ := db.Connect("root:root@tcp(127.0.0.1:3306)/amionline")
 	defer db.Disconnect(d)
 	router.Start()
