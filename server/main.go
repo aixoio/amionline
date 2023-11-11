36f9c6877e03e2929e577b10a3a1b20900181aa9
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-12 10:25:02 +1100

    Load config

--- server/main.go
+++ server/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/aixoio/amionline/server/config"
+)
+
 func main() {
 	
+	config, err := config.LoadConfig("env.json")
+	if err != nil {
+		fmt.Println("Failed to load config file with error:", err.Error())
+		return
+	}
+
 }
