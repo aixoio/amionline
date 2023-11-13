004eeb558b8c0b71b35b2e349328f08980ab200b
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-13 19:38:42 +1100

    Make pinger load config

---
+++ pinger/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/aixoio/amionline/logger"
+)
+
+type ConfigData struct {
+	TargetIP string `json:"target_ip"`
+	IntervalSeconds uint64 `json:"interval_seconds"`
+}
+
+func LoadConfig(path string) (*ConfigData, error) {
+	logger.Info().Println("Loading config file", path)
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config ConfigData
+	json_err := json.Unmarshal(bytes, &config)
+	if json_err != nil {
+		return nil, json_err
+	}
+
+	return &config, nil
+}
--- pinger/main.go
+++ pinger/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "github.com/aixoio/amionline/logger"
+import (
+	"github.com/aixoio/amionline/logger"
+	"github.com/aixoio/amionline/pinger/config"
+)
 
 func main() {
 	logger.Init()
 
+	con, err := config.LoadConfig("env.json")
+	if err != nil {
+		logger.Error().Println("Failed to load config file with error:", err.Error())
+		return
+	}
+
+	logger.Info().Println(con)
+	
 	logger.Info().Println("Hello world!")
 }
