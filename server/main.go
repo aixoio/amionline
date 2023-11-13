e2f3d934d746d0b2fda2332934dc2bba6cb3c069
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-13 16:02:44 +1100

    Add logging to main.go

--- server/main.go
+++ server/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aixoio/amionline/server/config"
 	"github.com/aixoio/amionline/server/db/mysql"
 	"github.com/aixoio/amionline/server/db/rediscache"
@@ -16,13 +14,13 @@ func main() {
 	
 	config, err := config.LoadConfig("env.json")
 	if err != nil {
-		fmt.Println("Failed to load config file with error:", err.Error())
+		logger.Error().Println("Failed to load config file with error:", err.Error())
 		return
 	}
 
 	db_connecter, err := mysql.Connect(config.Mysqldb)
 	if err != nil {
-		fmt.Println("Failed to connect to database with error:", err.Error())
+		logger.Error().Println("Failed to connect to database with error:", err.Error())
 	}
 	defer mysql.Disconnect(db_connecter)
 
