9c8b7abb35c8a201455cc3323f4099c2136a7cc3
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-17 18:18:40 +1100

    Add stats page

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -1,12 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
       component: () => import("@/views/HomeView.vue")
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: () => import("@/views/StatsView.vue")
     },
   ]
 })
