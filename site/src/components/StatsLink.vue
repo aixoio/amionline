21514d4af6761d36fc63f744f60c318968c1a404
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-17 21:33:05 +1100

    Add super basic stats page

---
+++ site/src/components/StatsLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="flex items-center">
+            <ChartBarSquareIcon class="w-8 h-8 text-gray-700 dark:text-gray-200 cursor-pointer" @click="go_to_stats_page"></ChartBarSquareIcon>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ChartBarSquareIcon } from "@heroicons/vue/24/outline"
+import { useRouter } from "vue-router";
+
+const router = useRouter()
+
+
+function go_to_stats_page() {
+    router.push({
+        name: "stats"
+    })
+}
+
+</script>
--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <nav class="md:flex block justify-start mt-5 border-b border-b-black/20 p-4">
-      <h1 class="title">Am I Online?</h1>
-      <div class="flex gap-4">
-        <OptionsMenu></OptionsMenu>
-        <ReloadButton></ReloadButton>
-        <span v-show="dataset != null && !dataset.cached"
-          class="bg-red-500 m-1.5 p-1 w-fit uppercase rounded-lg text-sm inline-block font-bold text-white">Uncached</span>
-      </div>
+    <nav class="flex w-full min-w-full border-b border-b-black/20 pb-4 justify-between">
+      <nav class="md:flex block justify-start mt-5 pt-4 pl-4 pr-4">
+        <h1 class="title">Am I Online?</h1>
+        <div class="flex gap-4">
+          <OptionsMenu></OptionsMenu>
+          <ReloadButton></ReloadButton>
+          <span v-show="dataset != null && !dataset.cached"
+            class="bg-red-500 flex items-center p-1 w-fit uppercase rounded-lg text-sm ml-2 mr-2 font-bold text-white">Uncached</span>
+        </div>
+      </nav>
+      <nav class="flex justify-end m-3">
+        <StatsLink class="flex items-center"></StatsLink>
+      </nav>
     </nav>
     <DataGraph v-if="canshowgraph" class="m-8 center"></DataGraph>
     <Loading v-if="!loaded" class="flex justify-center items-center h-full mt-56"></Loading>
@@ -19,6 +24,7 @@ import DataGraph from '@/components/DataGraph.vue';
 import OptionsMenu from '@/components/OptionsMenu.vue';
 import ReloadButton from '@/components/ReloadButton.vue';
 import Loading from '@/components/Loading.vue';
+import StatsLink from '@/components/StatsLink.vue';
 import { storeToRefs } from 'pinia';
 import { onMounted, ref, watch } from 'vue';
 import { get_last_20_events } from '../assets/ts/api';
