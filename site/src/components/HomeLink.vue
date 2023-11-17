a5931ed2538d9574ba5b583eba934f24cc05ef09
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-17 21:47:23 +1100

    Add better stats page

---
+++ site/src/components/HomeLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="flex items-center">
+            <HomeIcon class="w-8 h-8 text-gray-700 dark:text-gray-200 cursor-pointer" @click="go_to_home_page"></HomeIcon>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { HomeIcon } from "@heroicons/vue/24/outline"
+import { useRouter } from "vue-router";
+
+const router = useRouter()
+
+
+function go_to_home_page() {
+    router.push({
+        name: "home"
+    })
+}
+
+</script>
--- site/src/views/StatsView.vue
+++ site/src/views/StatsView.vue
@@ -1,5 +1,59 @@
 <template>
     <div>
-        <h1 class="title">stats</h1>
+        <nav class="flex w-full min-w-full border-b border-b-black/20 pb-4 justify-between">
+            <nav class="md:flex block justify-start mt-5 pt-4 pl-4 pr-4">
+                <h1 class="title">Am I Online? - Statistics</h1>
+                <div class="flex gap-4">
+                    <OptionsMenu></OptionsMenu>
+                    <ReloadButton></ReloadButton>
+                    <span class="text flex items-center">{{ get_event_name }}</span>
+                    <span v-show="dataset != null && !dataset.cached"
+                        class="bg-red-500 flex items-center p-1 w-fit uppercase rounded-lg text-sm ml-2 mr-2 font-bold text-white">Uncached</span>
+                </div>
+            </nav>
+            <nav class="flex justify-end m-3">
+                <HomeLink class="flex items-center"></HomeLink>
+            </nav>
+        </nav>
+        <Loading v-if="!loaded" class="flex justify-center items-center h-full mt-56"></Loading>
     </div>
 </template>
+
+<script setup lang="ts">
+import OptionsMenu from '@/components/OptionsMenu.vue';
+import HomeLink from "@/components/HomeLink.vue"
+import ReloadButton from '@/components/ReloadButton.vue';
+import Loading from '@/components/Loading.vue';
+import { storeToRefs } from "pinia"
+import { useDataStore } from "../stores/datastore"
+import { computed, onMounted } from 'vue';
+import { get_last_20_events } from '../assets/ts/api';
+
+const datastore = useDataStore()
+const { loaded, dataset } = storeToRefs(datastore)
+
+onMounted(async () => {
+  const data = await get_last_20_events()
+  datastore.dataset = data
+  datastore.loaded = true
+  datastore.store_type = "20"
+})
+
+const get_event_name = computed(() => {
+    switch (datastore.store_type) {
+        case "20":
+            return "Last 20 events"
+            break;
+        case "un20":
+            return "Last 20 events"
+            break;
+        case "all":
+            return "All events"
+            break;
+        case "unall":
+            return "All events"
+            break;
+    }
+})
+
+</script>
