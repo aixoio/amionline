333de79987a7f661e1e4911b79779a8ce7f5305a
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-18 22:26:36 +1100

    Add /health

--- server/router/router.go
+++ server/router/router.go
@@ -18,6 +18,11 @@ func Start(db_connecter *sql.DB, redis_client *redis.Client, config_data *config
 		w.Write([]byte("Hello world!"))
 	})
 
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		logger.Info().Printf("Handling request to %s from %s\n", r.URL.Path, r.RemoteAddr)
+		w.Write([]byte("I am alive!"))
+	})
+
 	register_log_event_request_handler(r, db_connecter, config_data, redis_client)
 	register_quit_request_handler(db_connecter, r, config_data, redis_client)
 	register_last_20_events_request_handler(r, db_connecter, redis_client)
